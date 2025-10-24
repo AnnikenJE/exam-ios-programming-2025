ae3c1925fc7d3dd5aecaef0bc31429c49b82c45f
Anniken <annikenje@gmail.com>
2025-10-24 12:00:53 +0200

    Initial Commit

---
+++ Beacon/BeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconApp.swift
+//  Beacon
+//
+//  Created by Anniken Jørgensen Edvardsen on 24/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
